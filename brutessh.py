2c254ab70c884067c22e02c988f1f053baa753ef
antogit-sys <antonioperrgiorgi@gmail.com>
2020-09-02 15:16:51 +0200

    code injection

--- brutessh.py
+++ brutessh.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
+#coding=utf-8
+
+import subprocess
 import os
+import sys
+
 def main():
 	os.system("clear;figlet -l Brutessh")
 	
@@ -7,22 +12,23 @@ def main():
 	ip=input("inserisci indirizzo ip per bruteforce: ")
 	user=input("inserisci utente: ")
 	key=input("inserisci parola chiave: ")
-	min=int(input("inserire min: "))
-	max=int(input("inserire max: "))
+	minn=str(input("inserire min: "))
+	maxx=str(input("inserire max: "))
 	
 	#creating wordlist
-	command="crunch "+str(min)+" "+str(max)+" "+str(key)+" > pass.txt" #pass
-	os.system(command)
+	#subprocess.call(['crunch',minn,maxx,key,'>','pass.txt'])
+	os.system("crunch "+minn+" "+maxx+" "+key+" > pass.txt") # /* BUG CODE INJECTION (‡▼益▼) */
 	
 	#Bruteforce Attack
-	os.system("sudo ncrack --user "+user+" -P pass.txt "+ip+":22") #ncrack --users admin -P pass.txt 192.168.1.X:22
+	#ncrack --users admin -P pass.txt 192.168.1.X:22
+	subprocess.call(["sudo","ncrack","--user",user,"-P","pass.txt",ip+":22"]) #fixed code injection
 	
 	risp=input("you want to connect locally with the ssh protocol?: ")
 	
-	if risp=="y" or "Y" or "S" or "s":
-		os.system("ssh -l "+user+" "+ip) #ssh -l root 192.168.1.X
-	else:
-		exit(1)
+	if risp=="N" or risp=="n":
+		sys.exit();
+	elif risp=="s" or risp=="S" or risp=="y" or risp=="Y":
+		subprocess.call(["ssh","-l",user,ip]) #ssh -l root 192.168.1.X | fixed code injection
 
 if __name__ == "__main__":
         main()
